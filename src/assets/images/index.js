8bed6863bb74bc52f7754104f13610d5da6bd956
creativehimel <himel.phy@bsmrstu.edu.bd>
2023-12-08 11:30:22 +0600

    Header Component Added

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import HomeInternet from './home-internet.png'
+import SMEInternet from './sme-internet.png'
+import GPSTracker from './gps-tracker.png'
+
+export { HomeInternet, SMEInternet, GPSTracker }
---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="bg-white shadow-sm py-3 px-[120px]">
+    <nav class="container mx-auto flex justify-between items-center">
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="../assets/images/logo-dark.png"
+        width="125"
+        height="125"
+      />
+      <ul class="flex items-center text-md gap-3 font-nunito font-[600]">
+        <li>
+          <RouterLink to="/">HOME INTERNET</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/">BUSINESS SOLUTION</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/">FTP</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/">TRACKER</RouterLink>
+        </li>
+      </ul>
+      <div class="flex items-center gap-3 font-nunito font-[600]">
+        <RouterLink
+          to="/sign-up"
+          class="bg-red-600 hover:bg-red-700 text-white px-7 py-2.5 rounded duration-300"
+          >Sign Up</RouterLink
+        >
+        <RouterLink
+          to="/pay-now"
+          class="bg-blue-600 hover:bg-blue-700 text-white px-7 py-2.5 rounded duration-300"
+          >Pay Now</RouterLink
+        >
+        <RouterLink
+          to="/login"
+          class="bg-blue-600 hover:bg-blue-700 text-white px-7 py-2.5 rounded duration-300"
+          >Login</RouterLink
+        >
+      </div>
+    </nav>
+  </header>
+</template>
+<style scoped></style>
